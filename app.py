68d9a0e909a8e0885c075220993152a6f49592b2
oneman72hourgamejam <oneman72hourgamejam@gmail.com>
2022-08-06 14:40:08 +0300

    refresh

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #basedir = os.path.abspath(os.path.dirname(__file__))
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lkvepagseawmgx:9ce6216230268ece23b4c0491ceed083a30062fc52d3aa96283c867605593446@ec2-34-232-191-133.compute-1.amazonaws.com:5432/dv3hh75la6ovr'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tlmuqwpijnmuxs:cf4b7a10cf14ca508169a8de688ceba07b921b27918d5569d0fc0c071ed25c01@ec2-3-213-228-206.compute-1.amazonaws.com:5432/d6q9mkqft7bo7o'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
@@ -17,8 +17,11 @@ db = SQLAlchemy(app)
 class MovieBase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    nika_rate = db.Column(db.Integer, default=0)
-    dima_rate = db.Column(db.Integer, default=0)
+    diml_rate = db.Column(db.Integer, default=0)
+    dimo_rate = db.Column(db.Integer, default=0)
+    andr_rate = db.Column(db.Integer, default=0)
+    artm_rate = db.Column(db.Integer, default=0)
+    maks_rate = db.Column(db.Integer, default=0)
     date_created = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
@@ -54,24 +57,52 @@ def index():
     if request.method == 'POST':
         
         movie_content = request.form['content']
-        movie_nika_rate = request.form['nika_rate']
-        movie_dima_rate = request.form['dima_rate']
+        movie_diml_rate = request.form['diml_rate']
+        movie_dimo_rate = request.form['dimo_rate']
+        movie_andr_rate = request.form['andr_rate']
+        movie_artm_rate = request.form['artm_rate']
+        movie_maks_rate = request.form['maks_rate']
         movie_date_created = request.form['date_created']
 
         if movie_content == '':
             return 'Помилка: заголовок не повинен бути порожнiм.'
-        
-        if int(movie_nika_rate) > 11:
-            movie_nika_rate = '10'
-        if int(movie_nika_rate) < 0:
-            movie_nika_rate = '0'
 
-        if int(movie_dima_rate) > 11:
-            movie_dima_rate = '10'
-        if int(movie_dima_rate) < 0:
-            movie_dima_rate = '0'
-        
-        new_movie = MovieBase(content=movie_content, nika_rate=movie_nika_rate, dima_rate=movie_dima_rate, date_created=movie_date_created)
+        if movie_diml_rate == "":
+            movie_diml_rate = '0'
+        if int(movie_diml_rate) > 11:
+            movie_diml_rate = '10'
+        if int(movie_diml_rate) < 0:
+            movie_diml_rate = '0'
+
+        if movie_dimo_rate == "":
+            movie_dimo_rate = '0'
+        if int(movie_dimo_rate) > 11:
+            movie_dimo_rate = '10'
+        if int(movie_dimo_rate) < 0:
+            movie_dimo_rate = '0'
+
+        if movie_andr_rate == "":
+            movie_andr_rate = '0'
+        if int(movie_andr_rate) > 11:
+            movie_andr_rate = '10'
+        if int(movie_andr_rate) < 0:
+            movie_andr_rate = '0'
+
+        if movie_artm_rate == "":
+            movie_artm_rate = '0'
+        if int(movie_artm_rate) > 11:
+            movie_artm_rate = '10'
+        if int(movie_artm_rate) < 0:
+            movie_artm_rate = '0'
+
+        if movie_maks_rate == "":
+            movie_maks_rate = '0'
+        if int(movie_maks_rate) > 11:
+            movie_maks_rate = '10'
+        if int(movie_maks_rate) < 0:
+            movie_maks_rate = '0'
+
+        new_movie = MovieBase(content=movie_content, diml_rate=movie_diml_rate, dimo_rate=movie_dimo_rate, andr_rate=movie_andr_rate, artm_rate=movie_artm_rate, maks_rate=movie_maks_rate, date_created=movie_date_created)
 
         try:
             db.session.add(new_movie)
@@ -114,22 +145,50 @@ def update(id):
 
     if request.method == 'POST':
         movie.content = request.form['content']
-        movie.nika_rate = request.form['nika_rate']
-        movie.dima_rate = request.form['dima_rate']
+        movie.diml_rate = request.form['diml_rate']
+        movie.dimo_rate = request.form['dimo_rate']
+        movie.andr_rate = request.form['andr_rate']
+        movie.artm_rate = request.form['artm_rate']
+        movie.maks_rate = request.form['maks_rate']
         movie.date_created = request.form['date_created']
 
         if movie.content == '':
             return 'Помилка: заголовок не повинен бути порожнiм.'
-            
-        if int(movie.nika_rate) > 11:
-            movie.nika_rate = '10'
-        if int(movie.nika_rate) < 0:
-            movie.nika_rate = '0'
 
-        if int(movie.dima_rate) > 11:
-            movie.dima_rate = '10'
-        if int(movie.dima_rate) < 0:
-            movie.dima_rate = '0'
+        if movie.diml_rate == "":
+            movie.diml_rate = '0'
+        if int(movie.diml_rate) > 11:
+            movie.diml_rate = '10'
+        if int(movie.diml_rate) < 0:
+            movie.diml_rate = '0'
+
+        if movie.dimo_rate == "":
+            movie.dimo_rate = '0'
+        if int(movie.dimo_rate) > 11:
+            movie.dimo_rate = '10'
+        if int(movie.dimo_rate) < 0:
+            movie.dimo_rate = '0'
+
+        if movie.andr_rate == "":
+            movie.andr_rate = '0'
+        if int(movie.andr_rate) > 11:
+            movie.andr_rate = '10'
+        if int(movie.andr_rate) < 0:
+            movie.andr_rate = '0'
+
+        if movie.artm_rate == "":
+            movie.artm_rate = '0'
+        if int(movie.artm_rate) > 11:
+            movie.artm_rate = '10'
+        if int(movie.artm_rate) < 0:
+            movie.artm_rate = '0'
+
+        if movie.maks_rate == "":
+            movie.maks_rate = '0'
+        if int(movie.maks_rate) > 11:
+            movie.maks_rate = '10'
+        if int(movie.maks_rate) < 0:
+            movie.maks_rate = '0'
 
         try:
             db.session.commit()
