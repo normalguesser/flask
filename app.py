39d25d72d2e75a42316e61dd4de48eb3772cb2e7
Fred <gocheckband@gmail.com>
2023-02-06 22:54:44 -0800

    serials

--- app.py
+++ app.py
@@ -6,15 +6,24 @@ import os
 import random
 
 app = Flask(__name__)
-#basedir = os.path.abspath(os.path.dirname(__file__))
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wufaaxewhrgdom:de1b7a20531948d0198c0762826ab1e7d19b8d297be5f962c741c9e79627242d@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dfr498nnlc2fim'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wufaaxewhrgdom:de1b7a20531948d0198c0762826ab1e7d19b8d297be5f962c741c9e79627242d@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dfr498nnlc2fim'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 class MovieBase(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    diml_rate = db.Column(db.Integer, default=0)
+    nast_rate = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+class SerialBase(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     diml_rate = db.Column(db.Integer, default=0)
@@ -41,7 +50,7 @@ def add():
             db.session.commit()
             return redirect('/')
         except:
-            return 'Помилка: не вдалось додати фiльм.'
+            return 'Помилка: не вдалось додати.'
 
     else:
         moviesToWatch = MovieToWatch.query.order_by(MovieToWatch.id.desc()).all()
@@ -79,13 +88,53 @@ def index():
             db.session.commit()
             return redirect('/')
         except:
-            return 'Помилка: не вдалось додати фiльм.'
+            return 'Помилка: не вдалось додати.'
 
     else:
         movies = MovieBase.query.order_by(MovieBase.id.desc()).all()
         moviesToWatch = MovieToWatch.query.order_by(MovieToWatch.id.desc()).all()
 
         return render_template('index.html', movies=movies, moviesToWatch=moviesToWatch)
+
+@app.route('/serials', methods=['POST', 'GET'])
+def serials():
+    if request.method == 'POST':
+        
+        movie_content = request.form['content']
+        movie_diml_rate = request.form['diml_rate']
+        movie_nast_rate = request.form['nast_rate']
+
+        if movie_content == '':
+            return 'Помилка: заголовок не повинен бути порожнiм.'
+
+        if movie_diml_rate == "":
+            movie_diml_rate = '0'
+        if int(movie_diml_rate) > 11:
+            movie_diml_rate = '10'
+        if int(movie_diml_rate) < 0:
+            movie_diml_rate = '0'
+
+        if movie_nast_rate == "":
+            movie_nast_rate = '0'
+        if int(movie_nast_rate) > 11:
+            movie_nast_rate = '10'
+        if int(movie_nast_rate) < 0:
+            movie_nast_rate = '0'
+
+        new_movie = SerialBase(content=movie_content, diml_rate=movie_diml_rate, nast_rate=movie_nast_rate)
+
+        try:
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect('/serials')
+        except:
+            return 'Помилка: не вдалось додати.'
+
+    else:
+        movies = SerialBase.query.order_by(SerialBase.id.desc()).all()
+        moviesToWatch = MovieToWatch.query.order_by(MovieToWatch.id.desc()).all()
+
+        return render_template('serials.html', movies=movies, moviesToWatch=moviesToWatch)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -143,6 +192,41 @@ def update(id):
 
     else:
         return render_template('update.html', movie=movie)
+
+@app.route('/updates/<int:id>', methods=['GET', 'POST'])
+def updates(id):
+    movie = SerialBase.query.get_or_404(id)
+
+    if request.method == 'POST':
+        movie.content = request.form['content']
+        movie.diml_rate = request.form['diml_rate']
+        movie.nast_rate = request.form['nast_rate']
+
+        if movie.content == '':
+            return 'Помилка: заголовок не повинен бути порожнiм.'
+
+        if movie.diml_rate == "":
+            movie.diml_rate = '0'
+        if int(movie.diml_rate) > 11:
+            movie.diml_rate = '10'
+        if int(movie.diml_rate) < 0:
+            movie.diml_rate = '0'
+
+        if movie.nast_rate == "":
+            movie.nast_rate = '0'
+        if int(movie.nast_rate) > 11:
+            movie.nast_rate = '10'
+        if int(movie.nast_rate) < 0:
+            movie.nast_rate = '0'
+
+        try:
+            db.session.commit()
+            return redirect('/serials')
+        except:
+            return 'Помилка: не вдалось оновити.'
+
+    else:
+        return render_template('updates.html', movie=movie)
 
 @app.route('/random')
 def randomMovie():
